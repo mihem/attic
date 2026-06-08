7b7fad5072cde34ce3f4bb730b11674d1514669e
mischko <mhem@mailbox.org>
2026-06-08 07:23:02 +0200

    updated to all bioc packages

--- gen_packages.R
+++ gen_packages.R
@@ -6,7 +6,7 @@
 # with # are comments). No inline comments allowed; blacklist.txt must contain
 # only package names (or full-line comments).
 
-bioc_n <- 1500
+bioc_n <- Inf
 
 # Read blacklist: strip full-line comments and everything after first whitespace
 lines <- readLines("blacklist.txt")
