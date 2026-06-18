15c2dea04b8113cbf85844cdb895b5fa76b028f9
mischko <mhem@mailbox.org>
2026-06-18 07:17:05 +0200

    increase cran to 15000

--- gen_packages.R
+++ gen_packages.R
@@ -7,7 +7,7 @@
 # only package names (or full-line comments).
 
 bioc_n <- Inf
-cran_n <- 10000
+cran_n <- 15000
 
 write_nix_list <- function(packages, out_path) {
   nix_names <- gsub("\\.", "_", packages)
