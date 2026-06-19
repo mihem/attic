a9a1cd00e415496450b73cce554a8bc8b8d5730b
mischko <mhem@mailbox.org>
2026-06-19 15:25:02 +0200

    cran all packages

--- gen_packages.R
+++ gen_packages.R
@@ -7,7 +7,7 @@
 # only package names (or full-line comments).
 
 bioc_n <- Inf
-cran_n <- 25000
+cran_n <- Inf
 
 write_nix_list <- function(packages, out_path) {
   nix_names <- gsub("\\.", "_", packages)
